9f9a112d8078998b1f5ffe6df8d9558bd0538176
NimiGames68 <204414320+NimiGames68@users.noreply.github.com>
2026-07-11 19:09:35 +0000

    Delete website directory

--- website/components/button.js
+++
@@ -1,9 +0,0 @@
-import styles from './button.module.css';
-
-export default function Button({ children, href }) {
-  return (
-    <a href={href} className={styles.button}>
-      {children}
-    </a>
-  );
-}
